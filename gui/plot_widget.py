3dc22245799e46376e14f545e229f48948a7259e
mrak-devlop <morok2devil@gmail.com>
2026-07-07 02:08:50 +0400

    v0.3.2

--- gui/plot_widget.py
+++ gui/plot_widget.py
@@ -1,24 +1,58 @@
 import pyqtgraph as pg
-from PySide6.QtWidgets import QVBoxLayout, QWidget
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import (
+    QFrame,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+)
 
 from models.channel import Channel
 
 
-class PlotWidget(QWidget):
-    """Виджет одного графика."""
+class PlotWidget(QFrame):
+    """Панель одного графика."""
+
+    closed = Signal(str)
 
     def __init__(self):
         super().__init__()
 
+        self.channel = None
+
+        self.setFrameShape(QFrame.Shape.StyledPanel)
+
+        self.setMinimumHeight(180)
+        self.setMaximumHeight(220)
+
         layout = QVBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setContentsMargins(2, 2, 2, 2)
+        layout.setSpacing(2)
+
+        #
+        # Верхняя панель
+        #
+
+        header = QHBoxLayout()
+
+        self.title = QLabel("")
+
+        self.close_button = QPushButton("✕")
+
+        self.close_button.setFixedSize(24, 24)
+
+        header.addWidget(self.title)
+
+        header.addStretch()
+
+        header.addWidget(self.close_button)
+
+        #
+        # График
+        #
 
         self.plot = pg.PlotWidget()
-
-        self.plot.setMouseEnabled(
-            x=True,
-            y=False,
-        )
 
         self.plot.showGrid(
             x=True,
@@ -26,23 +60,28 @@ class PlotWidget(QWidget):
             alpha=0.3,
         )
 
+        self.plot.setMouseEnabled(
+            x=True,
+            y=False,
+        )
+
         self.plot.setMenuEnabled(False)
 
-        self.plot.showGrid(x=True, y=True)
-
-        self.plot.setBackground("w")
-
-        self.setMinimumHeight(180)
-
-        self.setMaximumHeight(220)
+        layout.addLayout(header)
 
         layout.addWidget(self.plot)
 
-    def clear(self):
+        self.close_button.clicked.connect(self.on_close)
 
-        self.plot.clear()
+    def show_channel(
+        self,
+        timestamps,
+        channel: Channel,
+    ):
 
-    def show_channel(self, timestamps, channel: Channel):
+        self.channel = channel
+
+        self.title.setText(channel.name)
 
         self.plot.clear()
 
@@ -52,8 +91,19 @@ class PlotWidget(QWidget):
             pen=pg.mkPen(width=2),
         )
 
-        self.plot.setTitle(channel.name)
+        self.plot.setLabel(
+            "left",
+            channel.name,
+        )
 
-        self.plot.setLabel("left", channel.name)
+        self.plot.setLabel(
+            "bottom",
+            "Time",
+        )
 
-        self.plot.setLabel("bottom", "Time")
+    def on_close(self):
+
+        if self.channel is None:
+            return
+
+        self.closed.emit(self.channel.name)
